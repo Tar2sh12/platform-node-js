6628e5c66e6df6cf258c628d36df1fbab1ac2739
mohamed tarek <mohamad1tarek1@gmail.com>
2024-06-02 01:42:01 +0300

    v1

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import express, { json } from 'express';
 const app = express();
 import { dbConnection } from './DB/connection.js';
 import userRouter from './src/modules/user/user.routes.js';
-import User from './DB/models/User.model.js';
-import Post from './DB/models/Post.model.js';
-import comment from './DB/models/comment.model.js';
 import cors from 'cors';
 app.use(cors());
 app.use(json())
